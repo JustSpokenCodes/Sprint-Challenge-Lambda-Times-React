75bdebfce691321effeb537247f4bba84ec8e60e
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-06-15 19:43:17 -0700

    now im done i think

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from "styled-components"
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+const TopStyle = styled.div `
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-content: center;
+  width: 100%;
+  border: 2px solid blue;
+  ${props => (props.type === "left" ? `background : yellow-green`: null)}`
 
 const TopBar = () => {
   return (
