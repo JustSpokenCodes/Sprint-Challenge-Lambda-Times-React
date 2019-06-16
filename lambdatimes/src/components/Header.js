909fbd55010f9615da09734c05ef11f6f2a215e8
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-06-15 19:41:24 -0700

    css in the code works so much cleanier

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import styled from "styled-components"
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+
+const HeaderStyle = styled.div `
+  display: inline-flex
+  flex-direction:row;
+  width: 100%;
+  justify-content: center
+`
+const SpanStyle = styled.span `
+  font size: 25px;
+  margin: 40px;`
+
+const Hstyle = styled.h1 `
+  font-size: 45px;
+  margin: 20px`
 
 const Header = () => {
   return (
