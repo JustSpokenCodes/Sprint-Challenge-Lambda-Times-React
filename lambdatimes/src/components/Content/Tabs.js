62af58e0192bd8cea4c926bd234a40e6bd0f3bb2
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-06-15 19:34:46 -0700

    working on tabs

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Tab from './Tab';
+import styled from "styled-components";
+
+const DivStyle = styled.div `
+  display: inline-flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-evenly;
+`
 
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {}
+        {props.tabs.map (tab => {
+          return (
+            <Tab 
+              id= {tab.id}
+              tab= {tab}
+              changeSelected= {props.changeSelected} />
+          )
+        })}
       </div>
     </div>
   );
